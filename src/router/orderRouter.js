1be33609547890c4b8d8a01492dadfcc6499b1e0
Dilip Kumar <dilipkumarconnect@gmail.com>
2024-12-31 15:34:14 +0530

    frontend file connect to git

--- src/router/orderRouter.js
+++
@@ -1,9 +0,0 @@
-import {authUser} from '../middleware/authMiddleware.js'
-import { Router } from "express";
-import {placeOrderController} from '../controllers/orderController.js'
-
-const router = Router()
-
-router.route("/placed").post(authUser,placeOrderController)
-
-export default router
